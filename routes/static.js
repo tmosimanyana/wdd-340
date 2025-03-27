908e9fdf11dbca830e00d12a0a044528a080a86f
Tinny Mosimanyana <mbothepha@gmail.com>
2025-03-27 14:53:58 +0200

    Set up static file serving for public assets in static.js

--- routes/static.js
+++ routes/static.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// Set up "public" folder and subfolders for static files
+router.use(express.static(path.join(__dirname, "../public")));
+router.use("/css", express.static(path.join(__dirname, "../public/css")));
+router.use("/js", express.static(path.join(__dirname, "../public/js")));
+router.use("/images", express.static(path.join(__dirname, "../public/images")));
+
+module.exports = router;
 
 
 
