5589a4c0b8ba8d7a71ae69cdda6bfaf9559ed3a7
Tinny Mosimanyana <mbothepha@gmail.com>
2025-03-27 14:57:26 +0200

    Import and use static routes in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const expressLayouts = require("express-ejs-layouts");
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const contactRoutes = require('./routes/contactRoutes');
+const staticRoutes = require('./routes/staticRoutes'); // Import static routes
 
 dotenv.config();
 connectDB();
@@ -10,17 +11,20 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+// Use Static Routes
+app.use(staticRoutes);
+
 // View Engine and Templates
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); // not at views root
+app.set("layout", "./layouts/layout");
 
 // Index route
 app.get("/", function(req, res) {
   res.render("index", { title: "Home" });
 });
 
-// Routes
+// API Routes
 app.use('/api/contacts', contactRoutes);
 
 const PORT = process.env.PORT || 5000;
