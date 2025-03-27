1312fe4f9dd506d7252bfadcdc7e761c013e0101
Tinny Mosimanyana <mbothepha@gmail.com>
2025-03-27 14:32:43 +0200

    Add index route and update index.ejs layout with header, navigation, and footer

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const expressLayouts = require("express-ejs-layouts");
-
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const contactRoutes = require('./routes/contactRoutes');
@@ -12,14 +10,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-
-
 // View Engine and Templates
-
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
 
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" });
+});
 
 // Routes
 app.use('/api/contacts', contactRoutes);
